045e501efc5080044cb82fda49a46da6daa4bac2
tamya <tamyacode@163.com>
2023-04-17 00:20:07 +0800

    添加代理更换逻辑，仅作框架参考未实现功能

--- govspider/middlewares.py
+++ govspider/middlewares.py
@@ -10,8 +10,16 @@ from scrapy.http import HtmlResponse
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
-from selenium import webdriver
 from selenium.common import TimeoutException
+import random
+import time
+# import redom_proxy  # 随机获取一个代理的方法
+from twisted.internet.error import TimeoutError, DNSLookupError, \
+    ConnectionRefusedError, ConnectionDone, ConnectError, \
+    ConnectionLost, TCPTimedOutError
+from scrapy.core.downloader.handlers.http11 import TunnelError
+from twisted.internet import defer
+from twisted.web.client import ResponseFailed
 
 
 class GovspiderSpiderMiddleware:
@@ -152,3 +160,77 @@ class UaMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+def redom_proxy():
+    """
+    随机获取一个代理的方法
+    :return:
+    """
+    return
+
+
+class ProxiesMiddleware:
+    ALL_EXCEPTIONS = (defer.TimeoutError, TimeoutError, DNSLookupError,
+                      ConnectionRefusedError, ConnectionDone, ConnectError,
+                      ConnectionLost, TCPTimedOutError, ResponseFailed,
+                      IOError, TunnelError)
+
+    def __init__(self):
+        self.proxy = redom_proxy()
+        self.count = 0
+        self.information = self.information_func()
+
+    def information_func(self):
+        """设定过期时间 逻辑 当前时间+7000秒+随机一个50到200秒，因为我用的redis存储的代理，
+             有代理失效逻辑，同时大量代理一起失效会存在问题"""
+        return time.time() + 7000 + random.randint(50, 200)
+
+    def agentExecutable(self, name):
+        if time.time() > self.information:
+            self.proxy = redom_proxy(spiderhost=name)
+            self.information = self.information_func()
+
+    def process_request(self, request, spider):
+        """
+        source:判定是哪个网站，进而获取这个网站的专用代理
+        """
+        source = request.meta.get("source", "xxx")
+        if self.count % 10000 == 0 and self.count != 0:
+            """另一种换代理逻辑，没有走到过这"""
+            spider.logger.info("[一万个请求啦换代理了]")
+            self.proxy = redom_proxy(spiderhost=source)
+        self.count += 1
+        # 下面是用来判断是否到时间该换代理
+        self.agentExecutable(source)
+        spider.logger.info("[request url]   {}".format(request.url))
+        spider.logger.info("[proxy]   {}".format(self.proxy))
+        request.meta["proxy"] = self.proxy
+
+    def process_response(self, request, response, spider):
+        if len(response.text) < 3000 or response.status in [403, 400, 405, 301, 302]:
+            source = request.meta.get("source", "xxx")
+            spider.logger.info("[此代理报错]   {}".format(self.proxy))
+            new_proxy = redom_proxy(spiderhost=source)
+            self.proxy = new_proxy
+            spider.logger.info("[更的的新代理为]   {}".format(self.proxy))
+            new_request = request.copy()
+            new_request_l = new_request.replace(url=request.url)
+            return new_request_l
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # 捕获几乎所有的异常
+        if isinstance(exception, self.ALL_EXCEPTIONS):
+            # 在日志中打印异常类型
+            source = request.meta.get("source", "xxx")
+            spider.logger.info("[Got exception]   {}".format(exception))
+            spider.logger.info("[需要更换代理重试]   {}".format(self.proxy))
+            new_proxy = redom_proxy(spiderhost=source)
+            self.proxy = new_proxy
+            spider.logger.info("[更换后的代理为]   {}".format(self.proxy))
+            new_request = request.copy()
+            new_request_l = new_request.replace(url=request.url)
+            return new_request_l
+        # 打印出未捕获到的异常
+        spider.logger.info("[not contained exception]   {}".format(exception))
